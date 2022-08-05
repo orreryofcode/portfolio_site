7d7f8c6c4766c22f16ba15def557a01d8c4ed194
Raul Menendez <orreryofcode>
2022-08-04 20:18:56 -0700

    Optimize header image for load faster. Remove routing for blog.

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-const posts = [
-  {
-    title: "My Start In Web Development",
-    body: ["", ""],
-  },
-];
+// const posts = [
+//   {
+//     title: "My Start In Web Development",
+//     body: ["", ""],
+//   },
+// ];
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -21,21 +21,21 @@ app.get("/works", (req, res) => {
   res.render("works");
 });
 
-app.get("/blog", (req, res) => {
-  res.render("blog", { blogContent: posts });
-});
+// app.get("/blog", (req, res) => {
+//   res.render("blog", { blogContent: posts });
+// });
 
-app.get("/blog/:postTitle", (req, res) => {
-  const requestedTitle = req.params.postTitle;
+// app.get("/blog/:postTitle", (req, res) => {
+//   const requestedTitle = req.params.postTitle;
 
-  posts.forEach((post) => {
-    const storedTitle = post.title;
+//   posts.forEach((post) => {
+//     const storedTitle = post.title;
 
-    if (storedTitle === requestedTitle) {
-      res.render("post", { title: post.title, body: post.body });
-    }
-  });
-});
+//     if (storedTitle === requestedTitle) {
+//       res.render("post", { title: post.title, body: post.body });
+//     }
+//   });
+// });
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");

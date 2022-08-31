26fd4e5a60daf6a4671224f25b12e05f46d4c744
Raul Menendez <orreryofcode@gmail.com>
2022-08-31 16:47:22 -0700

    update styling and links

--- app.js
+++ app.js
@@ -6,13 +6,6 @@ const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-// const posts = [
-//   {
-//     title: "My Start In Web Development",
-//     body: ["", ""],
-//   },
-// ];
-
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -20,22 +13,6 @@ app.get("/", (req, res) => {
 app.get("/works", (req, res) => {
   res.render("works");
 });
-
-// app.get("/blog", (req, res) => {
-//   res.render("blog", { blogContent: posts });
-// });
-
-// app.get("/blog/:postTitle", (req, res) => {
-//   const requestedTitle = req.params.postTitle;
-
-//   posts.forEach((post) => {
-//     const storedTitle = post.title;
-
-//     if (storedTitle === requestedTitle) {
-//       res.render("post", { title: post.title, body: post.body });
-//     }
-//   });
-// });
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");

2dd768b5458051d43a626bf9621b10d35c707ee7
Raul Menendez <orreryofcode@gmail.com>
2022-09-02 18:43:41 -0700

    prepare site for deployment

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -14,6 +17,6 @@ app.get("/works", (req, res) => {
   res.render("works");
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(PORT || 3000, () => {
+  console.log("Server started");
 });

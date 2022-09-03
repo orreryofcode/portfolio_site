2d49cbbbfffbda11968b5493984987b55ecf9115
Raul Menendez <orreryofcode@gmail.com>
2022-09-02 22:07:42 -0700

    clean up files

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const ejs = require("ejs");
-
+const PORT = process.env.PORT;
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-
-const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -19,3 +17,5 @@ app.get("/works", (req, res) => {
 app.listen(PORT || 3000, () => {
   console.log("Server started");
 });
+
+module.exports = app;
